403bca7759191b7f1b76dc1a95a339538b425953
Greg Medlock <gmedlo@gmail.com>
2015-11-15 11:51:38 -0500

    Updated readme and analysis script
    Found objective parsing error in cobrapy, attempted setting multiple
    objectives

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# community_modeling readme
+community_modeling
 
 community_modeling is a project for BME8315, in which we will build a model of
 metabolism in a community consisting of S. cerevisiae and T. maritima.
--- bin/beer_community.py
+++ bin/beer_community.py
@@ -9,4 +9,70 @@ import cobra.flux_analysis.variability
 import libsbml
 import os
 
-def combine_models(model1,model2):
+#def combine_models(model1,model2):
+
+def essential_exchanges(model, exchange_list):
+    essential = cobra.flux_analysis.essentiality.assess_medium_component_essentiality(\
+                    model, the_components = exchange_list, solver = 'cglpk')
+    return essential
+
+def get_exchange_list(bigg_model):
+    exchanges = []
+    for reaction in bigg_model.reactions:
+        #print reaction.id.endswith('t')
+        if reaction.id.endswith('t'):
+            print reaction.id
+            exchanges.append(reaction.id)
+    return exchanges
+
+def open_exchanges(model, exchange_list):
+    for reaction in model.reactions:
+        if reaction.id in exchange_list:
+            reaction.lower_bound = -10
+            #reaction.upper_bound = 1000
+
+
+os.chdir('../models')
+yeast_file = 'iMM904.xml'
+maritima_file = 'iLJ478.xml'
+yeast_path = '/'.join([os.getcwd(),yeast_file])
+maritima_path = '/'.join([os.getcwd(),maritima_file])
+yeast_model = cobra.io.read_sbml_model(yeast_path)
+maritima_model = cobra.io.read_sbml_model(maritima_path)
+os.chdir('..')
+
+print(yeast_model.optimize())
+print(maritima_model.optimize())
+print("--------------------------------------------------------------")
+print("Finding exchange reactions")
+yeast_exchanges = get_exchange_list(yeast_model)
+maritima_exchanges = get_exchange_list(maritima_model)
+
+print maritima_exchanges
+print yeast_exchanges
+
+# print("--------------------------------------------------------------")
+# print("Determining essential exchange reactions")
+# yeast_essential = essential_exchanges(yeast_model, yeast_exchanges)
+# maritima_essential = essential_exchanges(maritima_model, maritima_exchanges)
+#
+# print("yeast essential exchanges")
+# print yeast_essential
+# print("maritima essential exchanges")
+# print maritima_essential
+
+# Open exchange reactions necessary for yeast, then open all yeast essential
+# exchanges in the maritima model
+open_exchanges(yeast_model, yeast_exchanges)
+open_exchanges(maritima_model, maritima_exchanges)
+#print(yeast_model.optimize())
+#print( [reaction.objective_coefficient for reaction in yeast_model.reactions] )
+#print (cobra.flux_analysis.reaction.assess(yeast_model,yeast_model.reactions.get_by_id('BIOMASS_SC5_notrace')))
+
+yeast_model.reactions.get_by_id('BIOMASS_SC5_notrace').objective_coefficient = 1
+single_objective_solution = yeast_model.optimize()
+print(single_objective_solution)
+yeast_model.reactions.get_by_id('ETOHt').objective_coefficient = 1
+print( [reaction.objective_coefficient for reaction in yeast_model.reactions] )
+multi_objective_solution = yeast_model.optimize()
+print(multi_objective_solution)
