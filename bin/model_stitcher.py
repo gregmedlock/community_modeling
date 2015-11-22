f2c9cd12c34edb5545b3daecda16f705cd7bc8c3
gregmedlock <gmedlo@gmail.com>
2015-11-22 12:55:47 -0500

    fixed missing exchange reactions in model adding

--- bin/model_stitcher.py
+++ bin/model_stitcher.py
@@ -9,7 +9,9 @@ import libsbml
 import os
 
 
-def stitch_models(model_1,model_2,model_suffixes=['_mod1','_mod2']):
+def stitch_models(base_model,add_model,model_suffixes=['_mod1','_mod2']):
+    model_1 = base_model.copy()
+    model_2 = add_model.copy()
     # get compartments and abbreviations for each model
     model_1_compartments = model_1.compartments
     model_2_compartments = model_1.compartments
@@ -33,6 +35,7 @@ def stitch_models(model_1,model_2,model_suffixes=['_mod1','_mod2']):
     model_1.compartments = model_1_new_compartments
     model_2_new_compartments['e'] = 'extracellular space'
     model_2.compartments = model_2_new_compartments
+
     # Loop through model 1 to rename compartments for every met, from old to new dict
     for metabolite in model_1.metabolites:
         if metabolite.compartment in model_1_abbreviation_mapping.keys():
@@ -41,24 +44,23 @@ def stitch_models(model_1,model_2,model_suffixes=['_mod1','_mod2']):
     for metabolite in model_2.metabolites:
         if metabolite.compartment in model_2_abbreviation_mapping.keys():
             metabolite.compartment = model_2_abbreviation_mapping[metabolite.compartment]
+    #print model_2.reactions.get_by_id('EX_ala__L_e').id
     # Add model suffix to every reaction in model 1 and 2
-    model_1_exchange_reactions = []
     for reaction in model_1.reactions:
         if reaction.id.startswith('EX_') == False: # Don't rename exchange reactions
             reaction.id = reaction.id + model_suffixes[0]
-        else:
-            model_1_exchange_reactions.append(reaction.id)
-    print model_1_exchange_reactions
+
     for reaction in model_2.reactions:
         if reaction.id.startswith('EX_') == False: # Don't rename exchange reactions
-            reaction.id = reaction.id + model_suffixes[1]
-        else:
-            if reaction.id in model_1_exchange_reactions: # remove exchanges present in other model
-                print reaction.id
-                model_2.remove_reactions([reaction.id])
-    # Add models
-    stitched_model = model_1 + model_2
-    return stitched_model
+            new_reaction = reaction.copy()
+            new_reaction.id = reaction.id + model_suffixes[1]
+            model_1.add_reaction(new_reaction)
+        else: # search for non-duplicate exchange reactions and add to model 1
+            if reaction.id not in [i.id for i in model_1.reactions]:
+                new_reaction = reaction.copy()
+                model_1.add_reaction(new_reaction)
+
+    return model_1
 
 
 os.chdir('../models')
@@ -66,8 +68,8 @@ yeast_file = 'iMM904.xml'
 maritima_file = 'iLJ478.xml'
 yeast_path = '/'.join([os.getcwd(),yeast_file])
 maritima_path = '/'.join([os.getcwd(),maritima_file])
-yeast_model = cobra.io.read_sbml_model(yeast_path)
-maritima_model = cobra.io.read_sbml_model(maritima_path)
+yeast_model = cobra.io.sbml3.read_sbml_model(yeast_path)
+maritima_model = cobra.io.sbml3.read_sbml_model(maritima_path)
 os.chdir('..')
 
 stitched_model = stitch_models(yeast_model,maritima_model)
